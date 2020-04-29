dac39e041cd740234127158aae614cc125d42927
michael-1979 <michaeljr@newman-consulting.cc>
2020-04-29 13:04:36 -0600

    Added NavBar

--- project/start.js
+++ project/start.js
@@ -1,22 +1,25 @@
-var express = require('express');
+var express = require("express");
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
+app.set("port", process.env.PORT || 5000);
 
 app.use(express.static(__dirname));
 
 // views is directory for all template files
-app.set('views', __dirname + '/html');
-app.set('view engine', 'ejs');
+app.set("views", __dirname + "/html");
+app.set("view engine", "ejs");
 
-app.get('/', function(request, response) {
-  response.render('pages/index');
+app.get("/", function (request, response) {
+  response.render("pages/index");
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+app.get("/api", function (request, response) {
+  response.render("pages/api");
 });
 
+app.listen(app.get("port"), function () {
+  console.log("Node app is running on port", app.get("port"));
+});
 
 // This file is what handles incoming requests and
 // serves files to the browser, or executes server-side code
